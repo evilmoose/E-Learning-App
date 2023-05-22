abb1fa937b13b87038973656f7f251b4ab2400a2
Evilmoose <ronnielopez@gmail.com>
2023-05-22 09:53:50 -0700

    Connecting spinner and api so spinner only works during api call

--- frontend/src/components/redux/reducers/index.jsx
+++ frontend/src/components/redux/reducers/index.jsx
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
 import courses from "./courseReducer.jsx";
 import authors from "./authorReducer.jsx"
+import apiCallStatusReducer from "./apiStatusReducer.jsx";
 
 const rootReducer = combineReducers({
   courses,
-  authors
+  authors,
+  apiCallStatusReducer
 });
 
 export default rootReducer;
